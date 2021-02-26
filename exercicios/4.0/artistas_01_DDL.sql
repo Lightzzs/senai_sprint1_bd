d92728666747aedc91be5371454b0c2afad64da6
VINICIUS <marvinicius78910@gmail.com>
2021-02-26 00:17:06 -0300

    Exercicio 4 completo

---
+++ exercicios/4.0/artistas_01_DDL.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE Optus;
+
+USE Optus;
+
+CREATE TABLE Artistas
+(
+	idArtista			INT PRIMARY KEY IDENTITY
+	,Nome				VARCHAR(200)NOT NULL
+);
+
+CREATE TABLE Albuns
+(
+	idAlbum				INT PRIMARY KEY IDENTITY
+	,idArtista			INT FOREIGN KEY REFERENCES Artistas (idArtista)
+	,Titulo				VARCHAR(200)NOT NULL
+	,DataLancamento		VARCHAR(250)NOT NULL
+	,Localizacao		VARCHAR(200)NOT NULL
+	,QtdMinutos			VARCHAR(250)NOT NULL
+	,Ativo				VARCHAR(250)NOT NULL
+);
+
+CREATE TABLE Estilos
+(
+	idEstilo			INT PRIMARY KEY IDENTITY
+	,Nome				VARCHAR(200)NOT NULL
+);
+
+CREATE TABLE AlbunsEstilos
+(
+	idAlbum				INT PRIMARY KEY IDENTITY
+	,idEstilo			INT FOREIGN KEY REFERENCES Estilos (idEstilo)
+);
+
+CREATE TABLE Usuarios
+(
+	Nome				VARCHAR(200)NOT NULL
+	,Email				VARCHAR(200)NOT NULL
+	,Senha				VARCHAR(250)NOT NULL
+	,Permissão			VARCHAR(250)NOT NULL
+);
+
---
+++ exercicios/4.0/artistas_03_DQL.sql
@@ -0,0 +1,11 @@
+USE Optus;
+
+SELECT * FROM Artistas
+
+SELECT * FROM Estilos
+
+SELECT * FROM AlbunsEstilos
+
+SELECT Titulo, DataLancamento, Localizacao, QtdMinutos, Ativo, idAlbum FROM Albuns
+
+SELECT Nome, Email, Senha, Permissão FROM Usuarios
