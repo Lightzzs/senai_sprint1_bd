cf81cd2587581949e06ac4cea2746b964358beb2
VINICIUS <marvinicius78910@gmail.com>
2021-02-26 23:41:37 -0300

    Arrumando erros

--- exercicios/1.3/clinica_02_DML.sql
+++ exercicios/1.3/clinica_02_DML.sql
@@ -9,25 +9,14 @@ VALUES			('Junior', '10/11/2018', 1, 1),
 				('Sammy', '16/06/2016', 1, 2 );
 
 INSERT INTO TiposPets(Descricao)
-VALUES				('Cachorro', 13)
-					,('Gato', 14)
-					,('Ave', 15)
-					,('Mamífero', 16)
-					,('Anfíbio', 17)
-					,('Peixe', 18);
-
+VALUES				('Cachorro')
+					,('Gato');
+				
 INSERT INTO Racas(Descricao, idTipoPet)
-VALUES			 ('Poodle', 13)
-				,('Labrador', 13)
-				,('SRD', 13)
-				,('Siamês', 14)
-				,('Raposa', 16)
-				,('Rã', 17)
-				,('Rapazinho', 15)
-				,('Remeiro', 18)
-				,('Rena', 16)
-				,('Rolinha', 15)
-				,('Rinoceronte', 16);
+VALUES			 ('Poodle', 1)
+				,('Labrador', 1)
+				,('SRD', 1)
+				,('Siamês', 2);
 
 INSERT INTO Atendimentos(Descricao, DataAtendimento, idVeterinario, idPet)
 VALUES					('Restam 10 dias de vida', '22/01/2019', 1, 1)
@@ -39,5 +28,5 @@ VALUES			 ('Paulo')
 				,('Odirlei');
 
 INSERT INTO Veterinarios(Nome, CRMV, idClinica)
-VALUES			 ('Saulo', '405', 4)
-				,('Caique', '705', 4);
+VALUES			 ('Saulo', '405', 1)
+				,('Caique', '705', 1);
